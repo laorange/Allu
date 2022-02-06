45f57057e7b325f5a46cdc3add6b7d7e11628599
辣橙 <2625835752@qq.com>
2022-02-06 17:30:22 +0800

    1.0.0.alpha02061729 优化首页

--- index/views.py
+++ index/views.py
@@ -18,23 +18,25 @@ def index_view(request):
 
     funcs: List[IndexFunction] = []
 
-    # semester_config = SemesterConfig.objects.get(config_id=1)
-    # # semester_config.current_period
-    # period_b = 1 - (semester_config.current_period % 2)
-    # # week = (semester_config.week1_monday_date - datetime.now()).min // 10080 + 1
-    # now = datetime.datetime.now()
-    # weekday = now.isoweekday()
-    # after = (datetime.datetime.now() - datetime.timedelta(days=weekday - 1)).strftime("%Y-%m-%d")
-    # before = (datetime.datetime.now() + datetime.timedelta(days=7 - weekday)).strftime("%Y-%m-%d")
-
-    # funcs += [
-    #     IndexFunction(f"/course/api/?semester={semester + period_b}&after={after}&before={before}",
-    #                   f"/static/index/timetable{_index + 1}.svg",
-    #                   semester_choice[semester - 1][1]) for _index, semester in enumerate(range(1, 12, 2))
-    # ]
+    semester_config = SemesterConfig.objects.get(config_id=1)
+    # semester_config.current_period
+    period_b = 1 - (semester_config.current_period % 2)
+    # week = (semester_config.week1_monday_date - datetime.now()).min // 10080 + 1
+    now = datetime.datetime.now()
+    weekday = now.isoweekday()
+    after = (datetime.datetime.now() - datetime.timedelta(days=weekday - 1)).strftime("%Y-%m-%d")
+    before = (datetime.datetime.now() + datetime.timedelta(days=7 - weekday)).strftime("%Y-%m-%d")
 
     funcs += [
-        IndexFunction("/course/", "/static/index/query.svg", "自定义查询<br>(施工中)"),
+        IndexFunction(f"/course/api/Course/?format=json&semester={semester + period_b}&after={after}&before={before}",
+                      f"/static/index/timetable{_index + 1}.svg",
+                      semester_choice[semester + period_b - 1][1]) for _index, semester in enumerate(range(1, 12, 2))
+    ]
+
+    funcs += [
+        IndexFunction("/", "/static/index/freeClassroom.svg", "空闲教室"),
+        IndexFunction("/", "/static/index/PDC.svg", "教学计划"),
+        IndexFunction("/", "/static/index/exam.svg", "考试安排"),
         IndexFunction("/admin/", "/static/index/admin.svg", "信息管理"),
         IndexFunction("/course/api/", "/static/index/API.svg", "API"),
         IndexFunction("/help/", "/static/index/info.svg", "使用帮助"),
