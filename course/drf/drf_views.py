f9fab65424009bae24bfd3d24b3c4b457047cb08
辣橙 <68316902+laorange@users.noreply.github.com>
2022-07-02 18:08:13 +0800

    backup-07021808

--- course/drf/drf_views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .filter import drf_containers
-
-router = routers.DefaultRouter()
-
-for container in drf_containers.values():
-    router.register(container.name, container.view_set, basename=container.name)
--- course/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, reverse, include
-from . import views
-from .drf.drf_views import router
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
